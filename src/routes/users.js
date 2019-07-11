5c932d69051df17b7567646f1ccfc0cc92782848
Derya O. Kurin <dozdemir663@gmail.com>
2019-07-11 21:47:09 +0300

    Implemeted a profile view

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,5 +11,6 @@ router.post("/users", validation.validateUsers, userController.createUser);
 router.get("/users/sign_in", userController.signInForm);
 router.post("/users/sign_in", validation.validateUsers, userController.signIn);
 router.get("/users/sign_out", userController.signOut);
+router.get("/users/:id", userController.show);
 
 module.exports = router;
